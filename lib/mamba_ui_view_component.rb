f55dc5b586f1a972652d51879ee4561c6092a508
Facundo Espinosa <facundo.espinosa@fing.edu.uy>
2023-04-28 16:23:29 -0300

    Initial commit

---
+++ lib/mamba_ui_view_component.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mamba_ui_view_component/version"
+
+module MambaUiViewComponent
+  class Error < StandardError; end
+  # Your code goes here...
+end
